5c624d1d1977dd79fb4410384343cd9dc3211543
shhslin <shenghsuan.lin@tum.de>
2023-08-07 17:17:55 +0200

    adapt to new python standard

--- ls/__init__.py
+++ ls/__init__.py
@@ -1,9 +1,9 @@
 """
 A Module for Line Search Methods.
 """
+__all__ = filter(lambda s:not s.startswith('_'), dir())
 
 from .linesearch import *
 from .pyswolfe   import *
 from .pymswolfe  import *
 
-__all__ = filter(lambda s:not s.startswith('_'), dir())
